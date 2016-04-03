608fbf8ff9f3c39a4980255f0bf0b372dbc10e83
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2016-04-03 19:08:20 +0200

    nvc0: add missing glMemoryBarrier bits
    This fixes a bunch of subtests of
    arb_shader_image_load_store-host-mem-barrier.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- src/gallium/drivers/nouveau/nvc0/nvc0_context.c
+++ src/gallium/drivers/nouveau/nvc0/nvc0_context.c
@@ -91,7 +91,14 @@ nvc0_memory_barrier(struct pipe_context *pipe, unsigned flags)
          }
       }
    }
-   if (flags & PIPE_BARRIER_SHADER_BUFFER) {
+
+   if ((flags & PIPE_BARRIER_SHADER_BUFFER) ||
+       (flags & PIPE_BARRIER_CONSTANT_BUFFER) ||
+       (flags & PIPE_BARRIER_INDEX_BUFFER) ||
+       (flags & PIPE_BARRIER_IMAGE) ||
+       (flags & PIPE_BARRIER_TEXTURE) ||
+       (flags & PIPE_BARRIER_VERTEX_BUFFER) ||
+       (flags & PIPE_BARRIER_STREAMOUT_BUFFER)) {
       IMMED_NVC0(push, NVC0_3D(MEM_BARRIER), 0x1011);
    }
 }
